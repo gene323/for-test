80c8986c607d588f7878c068c2aef3599d3311a7
Gene Ho <geneho0323@gmail.com>
2024-03-16 11:56:44 +0800

    first commit

---
+++ app.py
@@ -0,0 +1,73 @@
+import os
+from PIL import Image
+import io
+from flask import Flask, request, jsonify
+import predict
+import json
+
+
+ALLOWED_EXTENSIONS = {'jpg', 'jpeg', 'png'}
+ALLOWED_MIME_TYPES = {'image/jpeg', 'image/png'}
+
+
+app = Flask(__name__)
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024  # 16MB
+app.json.sort_keys = False
+
+
+@app.route('/')
+def index():
+    return '''
+    <!DOCTYPE HTML>
+    <html>
+        <head>
+            <meta charset='UTF-8'>
+        </head>
+        <body>
+            <form method='post' enctype='multipart/form-data' action='api/v2'>
+                <input type='file' name='file'>
+                <input type='submit' value='upload'>
+            </form>
+        </body>
+    </html>
+    '''
+
+@app.route('/api/v2', methods=['POST'])
+def apiv2():
+    if 'file' in request.files:
+        file = request.files['file']
+        file.save(f'./uploaded/1.jpg')
+        ret = predict.predict(f'./uploaded/1.jpg')
+        os.remove(f'./uploaded/1.jpg')
+
+        return jsonify({'probs': ret})
+
+
+
+@app.route('/api', methods=['POST'])
+def api():
+    file = Image.open(io.BytesIO(request.files['file'].read()))
+
+    if file.format == 'JPEG':
+        file.save(f'./uploaded/1.jpg')
+        ret = predict.predict(f'./uploaded/1.jpg')
+        os.remove(f'./uploaded/1.jpg')
+    elif file.format == 'PNG':
+        file.save(f'./uploaded/1.png')
+        ret = predict.predict(f'./uploaded/1.png')
+        os.remove(f'./uploaded/1.png')
+    
+    with open('data.json', 'w') as f:
+        json.dump(ret, f)
+    response = jsonify({'probs': ret})
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
+
+def is_allowed_file(ext):
+    if ext in ALLOWED_EXTENSIONS:
+        return True
+    return False
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=8000, debug=True)
---
+++ predict.py
@@ -0,0 +1,23 @@
+from ultralytics import YOLO
+import matplotlib.pyplot as plt
+
+def predict(filename):
+	model = YOLO("best.pt")
+	results = model([filename])
+
+	classTop5 = [results[0].names[i] for i in results[0].probs.top5]
+	probsTop5 = results[0].probs.top5conf.numpy()
+
+	bars = plt.barh(classTop5, probsTop5)
+	plt.title(filename)
+	plt.ylabel('class')
+	plt.xlabel('probability')
+	plt.bar_label(bars)
+	plt.tight_layout()
+	plt.savefig('./static/images/bar.png')
+	plt.close('all')
+
+	res = {}
+	for i in range(5):
+		res[classTop5[i]] = str(probsTop5[i])
+	return res
